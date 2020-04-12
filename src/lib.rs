f630fe8a77cc4e5cf29840bb48a984b70e3c21ee
toku-sa-n <tokusan441@gmail.com>
2020-04-12 14:13:52 +0900

    [Fix]Not clearing bits correctly
    Use &= not to hurt other bits.

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ pub fn set(start_byte: usize, start_bit: usize, num_of_bits: usize) -> () {
 /// ```
 pub fn clear(start_byte: usize, start_bit: usize, num_of_bits: usize) -> () {
     unsafe {
-        *(start_byte as *mut u8) =
+        *(start_byte as *mut u8) &=
             !((1 << (start_bit + num_of_bits)) as u16 - (1 << start_bit) as u16) as u8;
     }
 }
