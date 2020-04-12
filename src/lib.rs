d71765632abc3c602113208b6f116ce4d962acbc
toku-sa-n <tokusan441@gmail.com>
2020-04-12 11:03:25 +0900

    [Add]Initial documentation of set_bits
    Example will be written later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+/// Set `num_of_bits` bits from the `start_bit`th bit of address `start_byte`.
+///
+/// `start_byte` may be more than 7.
+///
+/// # Examples
+///
+/// Set 3 bits from the 2nd bit of specified address.
+///
+/// ```
+/// ```
+fn set_bits(start_byte: usize, start_bit: usize, num_of_bits: usize) -> () {}
+
 #[cfg(test)]
 mod tests {
     #[test]
