f0d81938ad00b8233c962c883c117b8ddf250c7f
toku-sa-n <tokusan441@gmail.com>
2020-04-12 17:59:10 +0900

    [Add]Initial definition of set_head_byte function
    To implement straddling_byte_boundaries function.

--- src/set.rs
+++ src/set.rs
@@ -5,8 +5,28 @@ pub fn within_a_byte(address: usize, start_bit: usize, num_of_bits: usize) -> ()
     };
 }
 
+fn set_head_byte(address: usize, start_bit: usize) -> () {}
+
 pub fn straddling_byte_boundaries(address: usize, start_bit: usize, num_of_bits: usize) -> () {
     unsafe {
         *(address as *mut u16) |= 0b1111111111000;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn set_head_byte() -> () {
+        let byte: Box<u32> = Box::new(0);
+        let ptr = Box::into_raw(byte);
+
+        set_head_byte(ptr as usize, 3);
+        unsafe {
+            assert_eq!(*ptr, 0b11111000);
+        }
+
+        let _byte = unsafe { Box::from_raw(ptr) };
     }
 }
