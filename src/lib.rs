e2b74b7470a8484bb538395f4fc5eae894ed9363
toku-sa-n <tokusan441@gmail.com>
2020-04-12 13:32:52 +0900

    [Add]Document of clear function

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,25 @@ pub fn set(start_byte: usize, start_bit: usize, num_of_bits: usize) -> () {
     };
 }
 
+/// Clear `num_of_bits` bits from the `start_bit`th bit of address `start_byte`.
+///
+/// `num_of_bits` may be more than 7.
+///
+/// # Examples
+///
+/// Clear 3 bits from the 2nd bit of specified address.
+///
+/// ```
+/// let byte: Box<u32> = Box::new(!0);
+/// let ptr = Box::into_raw(byte);
+///
+/// set_bits::clear(ptr as usize, 2, 3);
+/// unsafe {
+///     assert_eq!(*ptr, 0b11100011);
+/// }
+///
+/// let byte = unsafe { Box::from_raw(ptr) };
+/// ```
 pub fn clear(start_byte: usize, start_bit: usize, num_of_bits: usize) -> () {}
 
 #[cfg(test)]
