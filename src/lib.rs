358833d58b56f6f9c7f2eaf762f5948d534547e5
toku-sa-n <tokusan441@gmail.com>
2020-04-12 11:16:53 +0900

    [Add]A test for set_bits_within_a_byte

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,18 @@ fn set_bits(start_byte: usize, start_bit: usize, num_of_bits: usize) -> () {}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn set_bits_within_a_byte() {
-        assert_eq!(2 + 2, 4);
+        let byte: Box<u32> = Box::new(0);
+        let ptr = Box::into_raw(byte);
+
+        set_bits(ptr as usize, 3, 2);
+        unsafe {
+            assert_eq!(*ptr, 0b11000);
+        }
+
+        let byte = unsafe { Box::from_raw(ptr) };
     }
 }
