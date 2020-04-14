c9bbd6b94859e76687ce82f1d4b8ee78376a51d1
toku-sa-n <tokusan441@gmail.com>
2020-04-14 20:25:59 +0900

    [Fix]Not clearing bits correctly
    when start_bit >= 8.

--- src/lib.rs
+++ src/lib.rs
@@ -49,8 +49,8 @@ pub fn set(address: usize, start_bit: usize, num_of_bits: usize) -> () {
 /// ```
 pub fn clear(address: usize, start_bit: usize, num_of_bits: usize) -> () {
     unsafe {
-        *(address as *mut u8) &=
-            !((1 << (start_bit + num_of_bits)) as u16 - (1 << start_bit) as u16) as u8;
+        *((address + start_bit / 8) as *mut u8) &=
+            !((1 << (start_bit % 8 + num_of_bits)) as u16 - (1 << (start_bit % 8)) as u16) as u8;
     }
 }
 
