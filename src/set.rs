4777af54490ce911faa5341f2dfe86e9b31526b3
toku-sa-n <tokusan441@gmail.com>
2020-04-14 21:31:43 +0900

    [Fix]Not setting head bits correctly
    when start_bit >= 8.

--- src/set.rs
+++ src/set.rs
@@ -7,7 +7,8 @@ pub fn within_a_byte(address: usize, start_bit: usize, num_of_bits: usize) -> ()
 
 fn set_head_byte(address: usize, start_bit: usize) -> () {
     unsafe {
-        *(address as *mut u8) |= ((1 << 8) as u16 - (1 << start_bit) as u16) as u8;
+        *((address + start_bit / 8) as *mut u8) |=
+            ((1 << 8) as u16 - (1 << (start_bit % 8)) as u16) as u8;
     }
 }
 
