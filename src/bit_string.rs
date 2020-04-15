b61a054aef4eaba949a4482641f68f49914491ff
toku-sa-n <tokusan441@gmail.com>
2020-04-15 23:46:28 +0900

    [Add]Define BitString::new()
    For general use.

--- src/bit_string.rs
+++ src/bit_string.rs
@@ -3,3 +3,13 @@ struct BitString {
     start_bit: usize,
     num_of_bits: usize,
 }
+
+impl BitString {
+    fn new(start_address: usize, start_bit: usize, num_of_bits: usize) -> Self {
+        Self {
+            start_address,
+            start_bit,
+            num_of_bits,
+        }
+    }
+}
