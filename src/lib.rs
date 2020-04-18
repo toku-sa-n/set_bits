33abc81f682eba4279376c4bf49056ac57444eb7
toku-sa-n <tokusan441@gmail.com>
2020-04-18 15:55:29 +0900

    [Change]Move benchmark test to benches dir
    Not to test bench tests when `cargo test`

--- src/lib.rs
+++ src/lib.rs
@@ -60,34 +60,3 @@ pub fn clear(address: usize, start_bit: usize, num_of_bits: usize) -> () {
         bit_operation::Operation::Clear,
     );
 }
-
-#[cfg(test)]
-mod tests {
-    extern crate std;
-    extern crate test;
-
-    use super::*;
-    use std::boxed::Box;
-    use test::Bencher;
-
-    #[bench]
-    fn bench_set_all_bits_of_large_region(b: &mut Bencher) -> () {
-        const BYTES_OF_POOL: usize = 2 << 20;
-        struct LargeRegion {
-            _pool: [u8; BYTES_OF_POOL],
-        };
-
-        let pool: Box<LargeRegion> = Box::new(LargeRegion {
-            _pool: [0; BYTES_OF_POOL],
-        });
-        let ptr = Box::into_raw(pool);
-
-        b.iter(|| {
-            set(ptr as usize, 0, BYTES_OF_POOL * 8);
-        });
-
-        unsafe {
-            let _release = Box::from_raw(ptr);
-        }
-    }
-}
