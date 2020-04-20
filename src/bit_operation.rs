ec958f53a3d7430059bff9718092c5d18a08e378
toku-sa-n <tokusan441@gmail.com>
2020-04-20 15:46:36 +0900

    [Change]Use operation.edit() to edit bits

--- src/bit_operation.rs
+++ src/bit_operation.rs
@@ -8,8 +8,8 @@ pub enum Operation {
 impl Operation {
     fn edit(&self, dest: DestPtr, bit_mask: SrcVal) -> () {
         match self {
-            Set => unsafe { *dest |= bit_mask },
-            Clear => unsafe { *dest &= !bit_mask },
+            Self::Set => unsafe { *dest |= bit_mask },
+            Self::Clear => unsafe { *dest &= !bit_mask },
         }
     }
 }
@@ -21,30 +21,8 @@ pub const NUM_OF_BITS: usize = 128;
 pub trait EditBitFunc = Fn(DestPtr, SrcVal) -> ();
 
 pub fn bit_operation(bit_string: bit_string::BitString, operation: Operation) -> () {
-    match operation {
-        Operation::Set => {
-            let set = |dest: DestPtr, bit_mask| unsafe {
-                *dest |= bit_mask;
-            };
-
-            edit_bit(bit_string, set);
-        }
-        Operation::Clear => {
-            let clear = |dest: DestPtr, bit_mask: SrcVal| unsafe {
-                *dest &= !bit_mask;
-            };
-
-            edit_bit(bit_string, clear);
-        }
-    }
-}
-
-fn edit_bit<T>(bit_string: bit_string::BitString, edit_func: T) -> ()
-where
-    T: EditBitFunc,
-{
     for i in 0..bit_string.num_of_sections() {
-        edit_func(
+        operation.edit(
             bit_string.get_address_of_section(i) as DestPtr,
             bit_string.bits_at_section(i),
         );
